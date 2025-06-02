3117fa14b7d3082ba2a54f2701a30099a3b4967d
Maniteja_Gaddam <happyfamily110705@gmail.com>
2025-06-03 01:06:07 +0530

    Time: 0 ms (100.00%) | Memory: 16.3 MB (8.50%) - LeetSync

--- 107-binary-tree-level-order-traversal-ii/binary-tree-level-order-traversal-ii.cpp
+++ 107-binary-tree-level-order-traversal-ii/binary-tree-level-order-traversal-ii.cpp
@@ -10,28 +10,59 @@
  * };
  */
 class Solution {
+
+private:
+    void solve(queue<TreeNode *> & q, vector<vector<int>> & ans){
+        int level_size = q.size();
+        vector<int> curr_level;
+        for(int idx = 0 ; idx < level_size ; idx++){
+            TreeNode * node = q.front();
+            q.pop();
+            curr_level.push_back(node -> val);
+            if(node -> left) q.push(node -> left);
+            if(node -> right) q.push(node -> right);
+        }
+        if(!q.empty()) solve(q, ans);
+        ans.push_back(curr_level);
+    }
+
 public:
     vector<vector<int>> levelOrderBottom(TreeNode* root) {
-        if(root == nullptr) return {};
+       
 
-        vector<vector<int>> levels;
-        queue<TreeNode *> q;
+    //    Recursive Code
+        // use head recurrssion to store the values bottom up
+        vector<vector<int>> ans;
+        if(root == nullptr) return ans;
+
+        queue<TreeNode * > q;
         q.push(root);
+        solve(q, ans);
+        return ans;
 
-        while(!q.empty()){
-            int level_size = q.size();
-            vector<int> curr_level;
-            for(int idx = 0 ; idx < level_size ; idx++){
-                TreeNode * node = q.front();
-                q.pop();
-                curr_level.push_back(node -> val);
-                if(node -> left) q.push(node -> left);
-                if(node -> right) q.push(node -> right);
-            }
-            levels.push_back(curr_level);
-        }
-        reverse(levels.begin(), levels.end());
 
-        return levels;
+    //    Common Soltion Given by Others
+        
+        // if(root == nullptr) return {};
+
+        // vector<vector<int>> levels;
+        // queue<TreeNode *> q;
+        // q.push(root);
+
+        // while(!q.empty()){
+        //     int level_size = q.size();
+        //     vector<int> curr_level;
+        //     for(int idx = 0 ; idx < level_size ; idx++){
+        //         TreeNode * node = q.front();
+        //         q.pop();
+        //         curr_level.push_back(node -> val);
+        //         if(node -> left) q.push(node -> left);
+        //         if(node -> right) q.push(node -> right);
+        //     }
+        //     levels.push_back(curr_level);
+        // }
+        // reverse(levels.begin(), levels.end());
+
+        // return levels;
     }
 };
