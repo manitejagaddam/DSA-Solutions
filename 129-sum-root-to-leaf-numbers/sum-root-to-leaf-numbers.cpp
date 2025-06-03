6b318379402fc8d63ff518bf3e270f882d790487
Maniteja_Gaddam <happyfamily110705@gmail.com>
2025-06-03 17:59:48 +0530

    Time: 0 ms (100.00%) | Memory: 12 MB (43.32%) - LeetSync

--- 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.cpp
+++ 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.cpp
@@ -12,24 +12,45 @@
 class Solution {
 
 private:
-    void inorder(TreeNode * root, int temp_val, int & ans){
+    void inorder(TreeNode * root, int curr_num, int & ans){
         if(!root) return;
-        temp_val = temp_val * 10 + root -> val;
-        if(! root -> left && !root -> right) ans += temp_val;
-        cout << temp_val << endl;
-        inorder(root -> left, temp_val, ans);
+        
+        curr_num = curr_num * 10 + root -> val;
+        
+        if(! root -> left && !root -> right) ans += curr_num;
+        
+        cout << curr_num << endl;
+        
+        inorder(root -> left, curr_num, ans);
 
-        inorder(root -> right, temp_val, ans);
+        inorder(root -> right, curr_num, ans);
+
+    }
+
+    int inorder_non_refence_var(TreeNode * root, int currNum){
+        if(!root) return 0;
+
+        currNum = currNum * 10 + root -> val;
+
+        if(!root -> left && !root -> right) return currNum;
+
+        int left = inorder_non_refence_var(root -> left, currNum);
+        int right = inorder_non_refence_var(root -> right, currNum);
+
+        return left+ right;
 
     }
 
 public:
     int sumNumbers(TreeNode* root) {
 
-        int ans = 0;
-        inorder(root, 0, ans);
+        // int ans = 0;
+        // inorder(root, 0, ans);
+        // return ans;
 
-        return ans;
+        // Without using the reference variable 
+        return inorder_non_refence_var(root, 0);
+
         
     }
 };
