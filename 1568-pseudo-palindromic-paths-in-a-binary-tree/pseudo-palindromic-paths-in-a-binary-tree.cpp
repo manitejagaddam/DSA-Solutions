0140183ab8dd193af2b2cd8acdbbc59ad884ea8f
Maniteja_Gaddam <happyfamily110705@gmail.com>
2025-06-11 00:30:09 +0530

    Time: 4 ms (88.44%) | Memory: 181.4 MB (81.12%) - LeetSync

---
+++ 1568-pseudo-palindromic-paths-in-a-binary-tree/pseudo-palindromic-paths-in-a-binary-tree.cpp
@@ -0,0 +1,94 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+
+private:
+
+    // bool isPalindrome(string s, int start, int end){
+    //     while(start < end){
+    //         if(s[start++] != s[end--]) return false;
+    //     }
+    //     return true;
+    // }
+
+    // bool checkPseudoPalindrome(string s){
+    //     int start = 0;
+    //     int end = s.length() - 1;
+    //     while(start < end){
+    //         if(s[start] != s[end]) {
+    //             return isPalindrome(s, start + 1, end) || isPalindrome(s, start, end - 1);
+    //         }
+    //         start++;
+    //         end--;
+    //     }
+    //     return true;
+    // }
+
+
+    bool checkPseudoPalindrome(string s){
+        vector<int> mpp(11, 0);
+        for(char i : s) mpp[i - '0']++;
+        int odd_count = 0;
+        // cout << "MPP : ";
+        // for(int i : mpp) cout << i << " ";
+        // cout << endl;
+        for(int i = 0 ; i < 10 ; i++){
+            if(mpp[i] % 2) odd_count++;
+            if(odd_count > 1) return false; 
+        }
+        return true;
+    }
+
+    int inorder(TreeNode * root, string s){
+        if(!root) return 0;
+        
+        s += to_string(root -> val);
+        cout << s << endl;
+        
+        if(!root -> left && !root -> right){
+            if(checkPseudoPalindrome(s)){
+                // cout << s << endl;
+                return 1;
+            }
+        }
+
+        int left = inorder(root -> left, s);
+        int right = inorder(root -> right, s);
+        return left + right;
+    }
+
+    int inorder_bits(TreeNode * root, int bit_num){
+        if(!root) return 0;
+
+        int on_bit = 1 << root -> val;
+        bit_num ^= on_bit;
+
+        if(!root -> left && !root -> right){
+            if((bit_num & (bit_num - 1)) == 0) return 1;
+            return 0;
+        }
+        int left = inorder_bits(root -> left, bit_num);
+        int right = inorder_bits(root -> right, bit_num);
+
+        return left + right;
+
+    }
+
+public:
+    int pseudoPalindromicPaths (TreeNode* root) {
+        // string s = "";
+        // return inorder(root, s);
+
+        return inorder_bits(root, 0);
+
+    }
+};
