fe885b9bad35e31b090b5cf233ee6047954be758
Maniteja_Gaddam <happyfamily110705@gmail.com>
2025-06-11 00:40:00 +0530

    Time: 2 ms (93.83%) | Memory: 181.3 MB (89.21%) - LeetSync

--- 1568-pseudo-palindromic-paths-in-a-binary-tree/pseudo-palindromic-paths-in-a-binary-tree.cpp
+++ 1568-pseudo-palindromic-paths-in-a-binary-tree/pseudo-palindromic-paths-in-a-binary-tree.cpp
@@ -33,6 +33,20 @@ private:
     //     return true;
     // }
 
+    bool isPesudoPalindromeNumber(long num){
+        int odd_count = 0;
+        while(num){
+            int digit = num % 10;
+            if(digit & 1) odd_count++;
+            num /= 10;
+        }
+        return odd_count <= 1;
+    }
+
+    int updateNum(long curr_num, int update_num){
+        return curr_num + pow(10, update_num);
+    }
+
 
     bool checkPseudoPalindrome(string s){
         vector<int> mpp(11, 0);
@@ -66,6 +80,22 @@ private:
         return left + right;
     }
 
+    int inorder_nums(TreeNode * root, long num){
+        if(!root) return 0;
+
+        int current_state_num = updateNum(num, root -> val);
+        if(!root -> left && !root -> right){
+            if(isPesudoPalindromeNumber(current_state_num)) return 1;
+            return 0;
+        }
+
+        int left = inorder_nums(root -> left, current_state_num);
+        int right = inorder_nums(root -> right, current_state_num);
+
+        return left + right;
+
+    }
+
     int inorder_bits(TreeNode * root, int bit_num){
         if(!root) return 0;
 
@@ -87,6 +117,8 @@ public:
     int pseudoPalindromicPaths (TreeNode* root) {
         // string s = "";
         // return inorder(root, s);
+
+        // return inorder_nums(root, 0);
 
         return inorder_bits(root, 0);
 
