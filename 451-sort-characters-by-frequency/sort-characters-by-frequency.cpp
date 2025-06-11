fd6f75f6ae979f963ca9e3b539968bbeaf6b9202
Maniteja_Gaddam <happyfamily110705@gmail.com>
2025-06-12 00:13:09 +0530

    Time: 4 ms (39.69%) | Memory: 16.6 MB (9.45%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -1,28 +1,59 @@
 class Solution {
 public:
     string frequencySort(string s) {
-        
 
-        // Using multimap 
+        
+        // Using Bucket Sort OPTIMAL APPROACH
 
         unordered_map<char, int> mpp;
         for(char ch : s) mpp[ch]++;
 
-        multimap<int, char, greater<int>> multi_mpp;
+        int n = s.length();
+
+        // creating a bucket with the size of the string
+        vector<vector<char>> bucket(n + 1);
+
         for(auto it : mpp){
             char ch = it.first;
             int count = it.second;
-            multi_mpp.insert({count, ch});
+            bucket[count].push_back(ch);
         }
 
         string ans = "";
-        for(auto it : multi_mpp){
-            int count = it.first;
-            char ch = it.second;
-            for(int i = 0 ; i < count ; i++) ans += ch;
+        for(int idx = n ; idx >= 0 ; idx--){
+            for(char ch : bucket[idx]){
+                for(int i = 0 ; i < idx ; i++){
+                    ans += ch;
+                }
+            }
         }
 
         return ans;
+
+
+
+
+
+        // // Using multimap 
+
+        // unordered_map<char, int> mpp;
+        // for(char ch : s) mpp[ch]++;
+
+        // multimap<int, char, greater<int>> multi_mpp;
+        // for(auto it : mpp){
+        //     char ch = it.first;
+        //     int count = it.second;
+        //     multi_mpp.insert({count, ch});
+        // }
+
+        // string ans = "";
+        // for(auto it : multi_mpp){
+        //     int count = it.first;
+        //     char ch = it.second;
+        //     for(int i = 0 ; i < count ; i++) ans += ch;
+        // }
+
+        // return ans;
         
     }
 };
