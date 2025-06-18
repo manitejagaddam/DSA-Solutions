c2fe0060b9c074d6ff57e6cf15a5e0842f5425cf
Maniteja_Gaddam <happyfamily110705@gmail.com>
2025-06-18 17:17:26 +0530

    Time: 0 ms (100.00%) | Memory: 99 MB (88.47%) - LeetSync

--- 1916-find-center-of-star-graph/find-center-of-star-graph.cpp
+++ 1916-find-center-of-star-graph/find-center-of-star-graph.cpp
@@ -1,16 +1,36 @@
 class Solution {
 public:
     int findCenter(vector<vector<int>>& edges) {
-        unordered_map<int, int> mpp;
-        for(auto it : edges){
-            int u = it[0];
-            int v = it[1];
-            mpp[u]++;
-            mpp[v]++;
-        }
-        for(auto it : mpp){
-            if(it.second == mpp.size() - 1) return it.first;
-        }
-        return -1;
+        // thsi is for the multiple star connections like having more nodes connected
+
+        // unordered_map<int, int> mpp;
+        // for(auto it : edges){
+        //     int u = it[0];
+        //     int v = it[1];
+        //     mpp[u]++;
+        //     mpp[v]++;
+        // }
+        // for(auto it : mpp){
+        //     if(it.second == mpp.size() - 1) return it.first;
+        // }
+        // return -1;
+
+        // OPTIMAL according to the question
+
+        // int u1 = edges[0][0];
+        // int u2 = edges[1][0];
+        // int v1 = edges[0][1];
+        // int v2 = edges[1][1];
+
+        // if(u1 == u2 || u1 == v2) return u1;
+        // return u2;
+
+        int a = edges[0][0], b = edges[0][1];
+        int c = edges[1][0], d = edges[1][1];
+        
+        // The common node in both edges is the center
+        if (a == c || a == d) return a;
+        return b;
+
     }
 };
