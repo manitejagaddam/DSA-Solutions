6d319e2ac826beccbaf98840d6b293e5a5fb81af
Maniteja_Gaddam <happyfamily110705@gmail.com>
2025-06-21 15:43:17 +0530

    Time: 7 ms (73.40%) | Memory: 185.4 MB (69.19%) - LeetSync

---
+++ 3420-find-occurrences-of-an-element-in-an-array/find-occurrences-of-an-element-in-an-array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> occurrencesOfElement(vector<int>& nums, vector<int>& queries, int x) {
+        int noe = nums.size();
+        vector<int> indexes;
+        for(int idx = 0 ; idx < noe ; idx++){
+            if(nums[idx] == x) indexes.push_back(idx);
+        }
+        vector<int> ans;
+        int qLen = queries.size();
+        for(int q_idx = 0 ; q_idx < qLen ; q_idx++){
+            if(queries[q_idx] > indexes.size()) ans.push_back(-1);
+            else ans.push_back(indexes[queries[q_idx] - 1]);
+        }
+        return ans;
+    }
+};
