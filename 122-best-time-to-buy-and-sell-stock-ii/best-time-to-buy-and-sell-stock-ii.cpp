b262834ad7c66b6857e36e41f0b163f3ef1b01c4
Maniteja_Gaddam <happyfamily110705@gmail.com>
2025-06-26 12:40:50 +0530

    Time: 0 ms (100.00%) | Memory: 17.1 MB (86.56%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,39 +1,11 @@
 class Solution {
-
-private:
-    int tabulation(vector<int> & prices){
-        int n = prices.size();
-        vector<vector<int>> dp(n + 1, vector<int> (2, 0));
-
-        for(int i = n - 1 ; i >= 0 ; i--){
-            for(int j = 0 ; j < 2 ; j++){
-                if(j){ // holding a stock
-                    int sell = prices[i] + dp[i + 1][0];
-                    int buy = dp[i + 1][1];
-                    dp[i][j] = max(sell, buy);
-                }else{ // not holding a stock
-                    int buy = dp[i + 1][0];
-                    int sell = -prices[i] + dp[i + 1][1];
-
-                    dp[i][j] = max(buy, sell);
-                }
-            }
-        }
-
-        return dp[0][0];
-
-    }
-
 public:
     int maxProfit(vector<int>& prices) {
-
-        return tabulation(prices);
-        
         int profit = 0;
-        for(int i = 1 ; i< prices.size() ; i++){
-            if(prices[i] > prices[i - 1]) profit += prices[i] - prices[i - 1];
+        int noe = prices.size();
+        for(int idx = 0 ; idx < noe - 1 ; idx++){
+            profit += prices[idx + 1] - prices[idx] > 0 ? prices[idx + 1] - prices[idx] : 0;
         }
-        
         return profit;
     }
 };
