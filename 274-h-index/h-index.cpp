f5af7368f1c023a304863a4fb188aecd05f1bdad
Maniteja_Gaddam <happyfamily110705@gmail.com>
2025-06-27 15:53:31 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (84.99%) - LeetSync

--- 274-h-index/h-index.cpp
+++ 274-h-index/h-index.cpp
@@ -1,12 +1,32 @@
 class Solution {
 public:
     int hIndex(vector<int>& citations) {
+        
+        // Using a Linear Search
+
+        // sort(citations.begin(), citations.end());
+        // int noe = citations.size();
+        // int h_idx = noe;
+        // for(int i = 0 ; i < noe ; i++, h_idx--){
+        //     if(citations[i] >= h_idx) return h_idx;
+        // }
+        // return h_idx;
+
+        // Using a BInary Search
+
         sort(citations.begin(), citations.end());
         int noe = citations.size();
-        int h_idx = noe;
-        for(int i = 0 ; i < noe ; i++, h_idx--){
-            if(citations[i] >= h_idx) return h_idx;
+        int start = 0 ;
+        int end = noe - 1;
+        int h_idx;
+        while(start <= end){
+            int mid = (start + end) / 2;
+            h_idx = noe - mid;
+            if(citations[mid] == h_idx) return h_idx;
+            else if(citations[mid] > h_idx){
+                end = mid - 1;
+            }else start = mid + 1;
         }
-        return h_idx;
+        return noe - start;
     }
 };
