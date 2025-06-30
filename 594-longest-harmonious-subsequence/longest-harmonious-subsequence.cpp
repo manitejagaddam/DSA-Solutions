3f692c6ee71dc82edf6370006b91b9ac2d7089d1
Maniteja_Gaddam <happyfamily110705@gmail.com>
2025-06-30 23:47:15 +0530

    Time: 39 ms (28.86%) | Memory: 43.9 MB (29.65%) - LeetSync

--- 594-longest-harmonious-subsequence/longest-harmonious-subsequence.cpp
+++ 594-longest-harmonious-subsequence/longest-harmonious-subsequence.cpp
@@ -9,5 +9,18 @@ public:
             if(mpp.find(num + 1) != mpp.end()) maxLen = maxLen < mpp[num] + mpp[num + 1] ? mpp[num] + mpp[num + 1] : maxLen;
         }
         return maxLen;
+
+
+        // unordered_map<int, int> mpp;
+        // for(int i : nums){
+        //     mpp[i]++;
+        //     mpp[i + 1]++;
+        // }
+        // if(mpp.size() == 2) return 0;
+        // int maxLen = 0;
+        // for(auto it : mpp){
+        //     maxLen = maxLen < it.second ? it.second : maxLen;
+        // }
+        // return maxLen > 1 ? maxLen : 0;
     }
 };
