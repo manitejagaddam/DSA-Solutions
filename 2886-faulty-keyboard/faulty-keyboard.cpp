e5832909bc62225a54f63194371fc3388797c513
Maniteja_Gaddam <happyfamily110705@gmail.com>
2025-07-01 09:46:20 +0530

    Time: 4 ms (29.29%) | Memory: 9.5 MB (36.09%) - LeetSync

--- 2886-faulty-keyboard/faulty-keyboard.cpp
+++ 2886-faulty-keyboard/faulty-keyboard.cpp
@@ -2,9 +2,15 @@ class Solution {
 public:
     string finalString(string s) {
         string ans = "";
-        for(char ch : s){
-            if (ch == 'i') reverse(ans.begin(), ans.end());
-            else ans += ch;
+        // for(char ch : s){
+        //     if (ch == 'i') reverse(ans.begin(), ans.end());
+        //     else ans += ch;
+        // }
+
+        int len = s.length();
+        for(int i = 0 ; i < len ; i++){
+            if(s[i] == 'i') reverse(ans.begin(), ans.end());
+            else ans += s[i];
         }
         return ans;
     }
