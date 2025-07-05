c5d03c74a19b7bb4f369588b0d23e019f3be1d7f
Maniteja_Gaddam <happyfamily110705@gmail.com>
2025-07-05 11:56:27 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (54.69%) - LeetSync

---
+++ 367-valid-perfect-square/valid-perfect-square.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        long long start = 1;
+        long long end = num;
+        while(start <= end){
+            long long mid = (start + end) / 2;
+            if(mid * mid == num) return true;
+            else if (mid * mid < num) start = mid + 1;
+            else end = mid - 1;  
+        }
+        return false;
+    }
+};
