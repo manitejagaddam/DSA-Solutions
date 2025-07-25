3d651d904aad8e14381cfbf15598f08e574552a8
Maniteja_Gaddam <happyfamily110705@gmail.com>
2025-07-25 10:35:56 +0530

    Time: 28 ms (5.91%) | Memory: 29.7 MB (37.84%) - LeetSync

---
+++ 3788-maximum-unique-subarray-sum-after-deletion/maximum-unique-subarray-sum-after-deletion.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int maxSum(vector<int>& nums) {
+        int maxSumVal = INT_MIN;
+        int sum = 0;
+        unordered_set<int> st;
+        bool foundPosVal = false;
+        for(int ele : nums){
+            if(ele > 0) {
+                foundPosVal = true;
+                break;
+            }
+        }
+        if(!foundPosVal){
+            for(int ele : nums){
+                maxSumVal = maxSumVal < ele ? ele : maxSumVal;
+            }
+            return maxSumVal;
+        }
+
+
+        for(int ele : nums){
+            if(ele < 0){
+                // st.clear();
+                // sum = 0;
+                continue;
+            }else if(st.count(ele) > 0) continue;
+            else{
+                sum += ele;
+                st.insert(ele);
+                maxSumVal = maxSumVal < sum ? sum : maxSumVal;
+            }
+            cout << ele << " "  << sum << " " << maxSumVal << endl;
+        }
+        return maxSumVal;
+    }
+};
