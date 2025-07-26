f293017f8ce773d106a6ea10f4993223fb8fecca
Maniteja_Gaddam <happyfamily110705@gmail.com>
2025-07-26 14:13:15 +0530

    Time: 11 ms (98.25%) | Memory: 36.8 MB (33.61%) - LeetSync

--- 1306-minimum-absolute-difference/minimum-absolute-difference.cpp
+++ 1306-minimum-absolute-difference/minimum-absolute-difference.cpp
@@ -5,15 +5,26 @@ public:
         vector<vector<int>> ans;
         int len = arr.size();
         int minDiff = INT_MAX;
-        for(int idx = 1 ; idx < len ; idx++){
-            minDiff = minDiff > arr[idx] - arr[idx - 1] ? arr[idx] - arr[idx - 1] : minDiff;
-        }
-        for(int idx = 1 ; idx < len ; idx++){
-            if(arr[idx] - arr[idx - 1] == minDiff){
-                ans.push_back({arr[idx - 1], arr[idx]});
-            }
-        }
+        // for(int idx = 1 ; idx < len ; idx++){
+        //     minDiff = minDiff > arr[idx] - arr[idx - 1] ? arr[idx] - arr[idx - 1] : minDiff;
+        // }
+        // for(int idx = 1 ; idx < len ; idx++){
+        //     if(arr[idx] - arr[idx - 1] == minDiff){
+        //         ans.push_back({arr[idx - 1], arr[idx]});
+        //     }
+        // }
+        // return ans;
 
+
+        for(int idx = 1 ; idx < len ; idx++){
+            int diff = arr[idx] - arr[idx - 1];
+            if(diff < minDiff){
+                ans.clear();
+                ans.push_back({arr[idx - 1], arr[idx]});
+                minDiff = diff;
+            }else if(diff == minDiff) ans.push_back({arr[idx - 1], arr[idx]});
+        }
         return ans;
+
     }
 };
