8fdf0e9844c643cdb8633b0040012b3e6095ee2b
Maniteja_Gaddam <happyfamily110705@gmail.com>
2025-08-09 15:30:27 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (17.71%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -1,21 +1,7 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        // int i = 0;
-        // int num = 0;
-        // while(n > 1 ){
-        //     if(n % 2 == 0){
-        //         n /= 2;
-        //     }else{
-        //         return false;
-        //     }
-        // }
-
-        // BIT MANUPLATION
-        if(n <= 0){
-            return false;
-        }
-        if ((n & n - 1) == 0) return true;
-        return false;
+        // cout << (n & (n - 1));
+        return n > 0 && ((n & (n - 1)) == 0);
     }
 };
