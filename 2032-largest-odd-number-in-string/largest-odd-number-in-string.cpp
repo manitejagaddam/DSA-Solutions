aa63640f766cd7aa17d83c6556e428684f0ee816
Maniteja_Gaddam <happyfamily110705@gmail.com>
2025-08-14 14:56:53 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (36.16%) - LeetSync

--- 2032-largest-odd-number-in-string/largest-odd-number-in-string.cpp
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     string largestOddNumber(string num) {
         int idx = num.size() - 1;
-        // while((num[idx] - '0') % 2 == 1) idx--;
-        for(; idx >= 0 ; idx--){
-            // cout << num[idx] - '0' << " ";
-            if((num[idx] - '0') % 2 == 1) break;
-            // cout << num[idx] - '0' << " ";
+        while(idx >= 0 && (num[idx] - '0') % 2 == 0) idx--;
+        // for(; idx >= 0 ; idx--){
+        //     // cout << num[idx] - '0' << " ";
+        //     if((num[idx] - '0') % 2 == 1) break;
+        //     // cout << num[idx] - '0' << " ";
 
-        }
+        // }
         return num.substr(0, idx + 1);
     }
 };
