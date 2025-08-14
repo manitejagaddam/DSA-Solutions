7fb1daf8c20c0439c55e9b008416f365f7e47d82
Maniteja_Gaddam <happyfamily110705@gmail.com>
2025-08-14 14:53:45 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (63.08%) - LeetSync

--- 2032-largest-odd-number-in-string/largest-odd-number-in-string.cpp
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.cpp
@@ -4,10 +4,10 @@ public:
         int idx = num.size() - 1;
         // while((num[idx] - '0') % 2 == 1) idx--;
         for(; idx >= 0 ; idx--){
-            cout << num[idx] - '0' << " ";
+            // cout << num[idx] - '0' << " ";
             if((num[idx] - '0') % 2 == 1) break;
-            cout << num[idx] - '0' << " ";
-            
+            // cout << num[idx] - '0' << " ";
+
         }
         return num.substr(0, idx + 1);
     }
