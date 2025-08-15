62338fc847a880b87a06ecdeb7e839a9335386aa
Maniteja_Gaddam <happyfamily110705@gmail.com>
2025-08-15 22:45:20 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (82.36%) - LeetSync

--- 342-power-of-four/power-of-four.cpp
+++ 342-power-of-four/power-of-four.cpp
@@ -1,14 +1,24 @@
 class Solution {
 public:
     bool isPowerOfFour(int n) {
-        int low = 0;
-        int high = n;
-        while(low <= high){
-            int mid = (low + high) / 2;
-            if(pow(4, mid) == n) return true;
-            else if(pow(4, mid) < n) low = mid + 1;
-            else high = mid - 1;
+        // int low = 0;
+        // int high = n;
+        // while(low <= high){
+        //     int mid = (low + high) / 2;
+        //     if(pow(4, mid) == n) return true;
+        //     else if(pow(4, mid) < n) low = mid + 1;
+        //     else high = mid - 1;
+        // }
+        // return false;
+
+        if(n == 0) return false;
+        int odd = 1;
+        while (odd < 33) {
+            if ( (n & (1 << (odd - 1))) == n )
+                return (n & (n - 1)) == 0;
+            odd += 2;
         }
         return false;
+
     }
 };
