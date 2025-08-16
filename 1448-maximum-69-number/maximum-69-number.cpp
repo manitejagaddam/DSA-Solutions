2a8f8b237aaeb53dcc4b2f6b3c1eb8c4f9dea7e2
Maniteja_Gaddam <happyfamily110705@gmail.com>
2025-08-16 14:28:06 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (71.54%) - LeetSync

--- 1448-maximum-69-number/maximum-69-number.cpp
+++ 1448-maximum-69-number/maximum-69-number.cpp
@@ -1,12 +1,45 @@
 class Solution {
+
+private:
+    std::vector<int> createNumArray(int num){
+        std::vector<int> ans;
+        while(num){
+            ans.push_back(num % 10);
+            num /= 10;
+        }
+        std::reverse(ans.begin(), ans.end());
+        return ans;
+    }
+
+    int makeNum(std::vector<int> & num_arr){
+        int num = 0;
+        int len = num_arr.size();
+        for(int num_idx = 0 ; num_idx < len ; num_idx++){
+            num = num * 10 + num_arr[num_idx];
+        }
+
+        return num;
+    }
+
+
 public:
     int maximum69Number (int num) {
-        string s = to_string(num);
-        int len = s.length();
-        for(int digit = 0 ; digit < len ; digit++){
-            if(s[digit] == '6'){
-                s[digit] = '9';
-                return stoi(s);
+        // std::string s = std::to_string(num);
+        // int len = s.length();
+        // for(int digit_idx = 0 ; digit_idx < len ; digit_idx++){
+        //     if(s[digit_idx] == '6'){
+        //         s[digit_idx] = '9';
+        //         return std::stoi(s);
+        //     }
+        // }
+        // return num;
+
+        std::vector<int> num_arr = createNumArray(num);
+        int len = num_arr.size();
+        for(int digit_idx = 0 ; digit_idx < len ; digit_idx++){
+            if(num_arr[digit_idx] == 6) {
+                num_arr[digit_idx] = 9;
+                return makeNum(num_arr);
             }
         }
         return num;
