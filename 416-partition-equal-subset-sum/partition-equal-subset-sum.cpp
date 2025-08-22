cca7a6b9c6691f373d76dfb8303c3f6ab7e94089
Maniteja_Gaddam <happyfamily110705@gmail.com>
2025-08-22 16:59:55 +0530

    Time: 91 ms (77.05%) | Memory: 13.2 MB (98.17%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -19,6 +19,18 @@ private:
         return dp[idx][tempSum] = memoization(nums, idx - 1, tempSum, target, dp) || memoization(nums, idx - 1, tempSum + nums[idx], target, dp);
     }
 
+    int tabulation(vector<int> & nums, int target){
+        int len = nums.size();
+        vector<bool> dp(target + 1, false);
+        dp[0] = true;
+        for(int idx = 0 ; idx < len ; idx++){
+            for(int tempSum = target ; tempSum >= nums[idx]; tempSum--){
+                dp[tempSum] = dp[tempSum] || dp[tempSum - nums[idx]]; 
+            }
+        }
+        return dp[target];
+    }
+
 public:
     bool canPartition(vector<int>& nums) {
         int len = nums.size();
@@ -29,8 +41,10 @@ public:
         int target = sum / 2;
 
         // return backtrack(nums, nums.size() - 1, 0, target);
-        vector<vector<int>> dp(len, vector<int>(target + 1, -1));
-        return memoization(nums, nums.size() - 1, 0, target, dp);
 
+        // vector<vector<int>> dp(len, vector<int>(target + 1, -1));
+        // return memoization(nums, nums.size() - 1, 0, target, dp);
+
+        return tabulation(nums, target);
     }
 };
