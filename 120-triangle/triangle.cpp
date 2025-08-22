9894030613fbefb0d63ef6e1f7462959f6d5fb02
Maniteja_Gaddam <happyfamily110705@gmail.com>
2025-08-22 14:58:32 +0530

    Time: 0 ms (100.00%) | Memory: 13.6 MB (6.18%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,46 +1,55 @@
 class Solution {
 
 private:
-    int recurssion(vector<vector<int>> & grid, int row, int col, int m){
-        int n = grid[row].size();
-        if(col >= n) return INT_MAX;
+    int backtrack(vector<vector<int>> & triangle, int row, int col, int size){
+        if(row == size - 1) return triangle[row][col];
+        if(row == size) return INT_MAX;
 
-        if(row == m - 1) return grid[row][col];
         
-        int down = grid[row][col] + recurssion(grid, row + 1, col, m);
-        int diagonal = grid[row][col] + recurssion(grid, row + 1, col + 1, m);
-
-        return min(down, diagonal);
+        return triangle[row][col] + min(backtrack(triangle, row + 1, col, size), backtrack(triangle, row + 1, col + 1, size));
     }
 
-    int memoization(vector<vector<int>> & grid, int row, int col, vector<vector<int>> & dp){
-        int m = grid.size();
-        // int n = grid[row].size();
-        int n = grid[row].size();
-
-        if(col >= n) return INT_MAX;
-
-        if(row == m - 1) return grid[row][col];
+    int memoization(vector<vector<int>> & triangle, int row, int col, int size, vector<vector<int>> & dp){
+        if(row == size - 1) return triangle[row][col];
+        if(row == size) return INT_MAX;
 
         if(dp[row][col] != -1) return dp[row][col];
+        
+        return triangle[row][col] + min(memoization(triangle, row + 1, col, size, dp), memoization(triangle, row + 1, col + 1, size, dp));
+    }
 
-        int down = grid[row][col] + memoization(grid, row + 1, col, dp);
-        int diagonal = grid[row][col] + memoization(grid, row + 1, col + 1, dp);
+    int tabulation(vector<vector<int>> & triangle){
+        int m = triangle.size();
+        vector<vector<int>> dp(m);
+        for(int i = 0 ; i < m ; i++){
+            dp[i] = vector<int>(i + 1, 0);
+        }
 
-        return dp[row][col] = min(down, diagonal);
+        for(int col = 0 ; col < m ; col++){
+            dp[m - 1][col] = triangle[m - 1][col];
+        }
+        
+        for(int row = m - 2 ; row >= 0 ; row--){
+            for(int col = 0 ; col <= row ; col++){
+                dp[row][col] = triangle[row][col] + min(dp[row + 1][col], dp[row + 1][col + 1]);
+            }
+        }
+
+        return dp[0][0];
     }
 
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
+        int m = triangle.size();
+        // return backtrack(triangle, 0, 0, m);
 
-        // // Recurssion Brute Force Method
-        // return recurssion(triangle, 0, 0, triangle.size());
+        // vector<vector<int>> dp(m);
+        // for (int i = 0; i < m; i++) {
+        //     dp[i] = vector<int>(i + 1, -1);
+        // }
+        // return memoization(triangle, 0, 0, m, dp);
 
-        // Memoization Optimal
-        vector<vector<int>> dp;
-        for (int i = 0; i < triangle.size(); ++i) {
-            dp.push_back(vector<int>(triangle[i].size(), -1));
-        }
-        return memoization(triangle, 0, 0, dp);
+        return tabulation(triangle);
+
     }
 };
