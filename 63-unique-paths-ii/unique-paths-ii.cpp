2d9ffa9b980642b7b56628f4a6f47410aba61c5e
Maniteja_Gaddam <happyfamily110705@gmail.com>
2025-08-22 13:47:22 +0530

    Time: 0 ms (100.00%) | Memory: 12 MB (28.53%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -21,6 +21,42 @@ private:
         return dp[row][col] = memoization(grid, row + 1, col, m, n, dp) + memoization(grid, row, col + 1, m, n, dp);
     }
 
+    int tabulation(vector<vector<int>> & grid, int m, int n){
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+
+        if (grid[0][0] == 1) return 0;
+
+        dp[0][0] = 1; 
+
+        for (int row = 1; row < m; row++) {
+            if (grid[row][0] == 0 && dp[row-1][0] == 1) {
+                dp[row][0] = 1;
+            } else {
+                dp[row][0] = 0; 
+            }
+        }
+
+        for (int col = 1; col < n; col++) {
+            if (grid[0][col] == 0 && dp[0][col-1] == 1) {
+                dp[0][col] = 1;
+            } else {
+                dp[0][col] = 0; 
+            }
+        }
+
+        for (int row = 1; row < m; row++) {
+            for (int col = 1; col < n; col++) {
+                if (grid[row][col] == 1) {
+                    dp[row][col] = 0; 
+                } else {
+                    dp[row][col] = dp[row-1][col] + dp[row][col-1];
+                }
+            }
+        }
+
+        return dp[m-1][n-1];
+    }
+
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int m = obstacleGrid.size();
@@ -28,7 +64,9 @@ public:
         if(obstacleGrid[m - 1][n - 1] == 1) return 0;
         // return backtrack(obstacleGrid, 0, 0, m, n);
 
-        vector<vector<int>> dp(m, vector<int>(n, -1));
-        return memoization(obstacleGrid, 0, 0, m, n, dp);
+        // vector<vector<int>> dp(m, vector<int>(n, -1));
+        // return memoization(obstacleGrid, 0, 0, m, n, dp);
+
+        return tabulation(obstacleGrid, m, n);
     }
 };
