8ca1f344582992034560fd97e8fcdf74d92a4aa3
Maniteja_Gaddam <happyfamily110705@gmail.com>
2025-08-22 23:16:07 +0530

    Time: 29 ms (39.39%) | Memory: 27.4 MB (68.59%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -18,14 +18,30 @@ private:
                                     memoization(s1, s2, idx1, idx2 - 1, dp));
     }
 
+    int tabulation(string & s1, string & s2){
+        int m = s1.length();
+        int n = s2.length();
+
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+        for(int idx1 = 1 ; idx1 <= m ; idx1++){
+            for(int idx2 = 1 ; idx2 <= n ; idx2++){
+                if(s1[idx1 - 1] == s2[idx2 - 1]) dp[idx1][idx2] = 1 + dp[idx1 - 1][idx2 - 1];
+                else dp[idx1][idx2] = max(dp[idx1 - 1][idx2], dp[idx1][idx2 - 1]);
+            }
+        }
+
+        return dp[m][n];
+    }
+
 public:
     int longestCommonSubsequence(string text1, string text2) {
         int idx1 = text1.length() - 1;
         int idx2 = text2.length() - 1;
 
         // return backtrack(text1, text2, idx1, idx2);
-        vector<vector<int>> dp(text1.size(), vector<int>(text2.size(), -1));
-        return memoization(text1, text2, idx1, idx2, dp);
+        // vector<vector<int>> dp(text1.size(), vector<int>(text2.size(), -1));
+        // return memoization(text1, text2, idx1, idx2, dp);
 
+        return tabulation(text1, text2);
     }
 };
