62fe403dee904e91968ade039c9c5f75afa38063
Maniteja_Gaddam <happyfamily110705@gmail.com>
2025-08-22 14:16:49 +0530

    Time: 5 ms (8.12%) | Memory: 16.4 MB (14.74%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -1,94 +1,29 @@
 class Solution {
 
 private:
-    int recurssion(vector<vector<int>> & grid, int row, int col){
-        int m = grid.size();
-        int n = grid[0].size();
+    int backtrack(vector<vector<int>> & grid, int row, int col, int m, int n){
+        if(row == m - 1 && col == n - 1) return grid[row][col];
+        if(row == m || col == n) return INT_MAX;
 
-        if(row == m - 1 && col == n - 1) return grid[m - 1][n - 1];
-        if(row >= m || col >= n) return INT_MAX;
-
-        int down = recurssion(grid, row + 1, col);
-        int right = recurssion(grid, row, col + 1); 
-
-        return grid[row][col] +  min(down, right);
+        return grid[row][col] + min(backtrack(grid, row + 1, col, m, n), backtrack(grid, row, col + 1, m, n));
     }
 
-    int memoization(vector<vector<int>> & grid, int row, int col, vector<vector<int>> & dp){
-        int m = grid.size();
-        int n = grid[0].size();
-
-        if(row >= m || col >= n) return INT_MAX;
-
-        if(row == m - 1 && col == n - 1) return grid[m - 1][n - 1];
+    int memoization(vector<vector<int>> & grid, int row, int col, int m, int n, vector<vector<int>> & dp){
+        if(row == m - 1 && col == n - 1) return grid[row][col];
+        if(row == m || col == n) return INT_MAX;
 
         if(dp[row][col] != -1) return dp[row][col];
 
-        int down = memoization(grid, row + 1, col, dp);
-        int right = memoization(grid, row, col + 1, dp);
-
-        return dp[row][col] = grid[row][col] + min(down, right);
-
-
-    }
-
-
-    int tabulation(vector<vector<int>> & grid){
-        int m = grid.size();
-        int n = grid[0].size();
-
-        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
-        
-        dp[0][0] = grid[0][0];
-
-         for(int j = 1; j < n; j++){
-            dp[0][j] = dp[0][j - 1] + grid[0][j];
-        }
-
-        for(int i = 1; i < m; i++){
-            dp[i][0] = dp[i - 1][0] + grid[i][0];
-        }
-
-        for(int i = 1 ; i < m ; i++){
-            for(int j = 1 ; j < n ; j++){
-                dp[i][j] = min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
-            }
-        }
-
-        return dp[m - 1][n - 1];
-
-    }
-
-
-    int tabulation_space_optimized(vector<vector<int>> & grid){
-        int m = grid.size();
-        int n = grid[0].size();
-
-        vector<int> prev(n, INT_MAX);
-        prev[0] = 0;
-
-        for(int i = 0 ; i < m ; i++){
-            vector<int> curr(n);
-            for(int j = 0 ; j < n ; j++){
-                if(j == 0) curr[j] = grid[i][j] + prev[j];
-                else curr[j] = min(curr[j - 1], prev[j]) + grid[i][j];
-            }
-            prev = curr;
-        }
-
-        return prev[n - 1];
+        return dp[row][col] = grid[row][col] + min(memoization(grid, row + 1, col, m, n, dp), memoization(grid, row, col + 1, m, n, dp));
     }
 
 public:
     int minPathSum(vector<vector<int>>& grid) {
-        // // BReute Force Method Recurssion
-        // return recurssion(grid, 0, 0);
+        int m = grid.size();
+        int n = grid[0].size();
+        // return backtrack(grid, 0, 0, m, n);
 
-        // vector<vector<int>> dp(grid.size(), vector<int>(grid[0].size(), -1));
-        // return memoization(grid, 0, 0, dp);
-
-        // return tabulation(grid);
-        
-        return tabulation_space_optimized(grid);
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        return memoization(grid, 0, 0, m, n, dp);
     }
 };
