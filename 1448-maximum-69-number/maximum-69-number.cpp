57ed0f096a927303af3fd16c21f6adf8e72b8120
Maniteja_Gaddam <happyfamily110705@gmail.com>
2025-08-31 13:56:04 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (72.52%) - LeetSync

--- 1448-maximum-69-number/maximum-69-number.cpp
+++ 1448-maximum-69-number/maximum-69-number.cpp
@@ -21,6 +21,15 @@ private:
         return num;
     }
 
+    int findNumLength(int num){
+        int len = 0;
+        while(num){
+            len++;
+            num /= 10;
+        }
+        return len;
+    }
+
 
 public:
     int maximum69Number (int num) {
@@ -34,14 +43,35 @@ public:
         // }
         // return num;
 
-        std::vector<int> num_arr = createNumArray(num);
-        int len = num_arr.size();
-        for(int digit_idx = 0 ; digit_idx < len ; digit_idx++){
-            if(num_arr[digit_idx] == 6) {
-                num_arr[digit_idx] = 9;
-                return makeNum(num_arr);
+        // std::vector<int> num_arr = createNumArray(num);
+        // int len = num_arr.size();
+        // for(int digit_idx = 0 ; digit_idx < len ; digit_idx++){
+        //     if(num_arr[digit_idx] == 6) {
+        //         num_arr[digit_idx] = 9;
+        //         return makeNum(num_arr);
+        //     }
+        // }
+        // return num;
+
+
+        int len = findNumLength(num) - 1;
+        // cout << len;
+        int temp = num;
+        int ans = 0;
+        bool flag = false;
+        while(temp){
+            int pow_val = std::pow(10, len);
+            int digit = (num / pow_val) % 10;
+            if(digit == 6 && !flag){
+                ans = ans * 10 + 9;
+                flag = true;
+            }else{
+                ans = ans * 10 + digit;
             }
+            // cout << len << " " << pow_val << " " << digit << " " << ans << endl;
+            temp /= 10;
+            len--;
         }
-        return num;
+        return ans;
     }
 };
