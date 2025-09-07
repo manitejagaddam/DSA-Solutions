39e334d4db22b09c558b993f405e69799b47b29f
Maniteja_Gaddam <happyfamily110705@gmail.com>
2025-09-07 16:52:18 +0530

    Time: 71 ms (38.30%) | Memory: 183.4 MB (12.04%) - LeetSync

--- 307-range-sum-query-mutable/range-sum-query-mutable.cpp
+++ 307-range-sum-query-mutable/range-sum-query-mutable.cpp
@@ -1,52 +1,54 @@
 class NumArray {
+
 private:
-    vector<int> seg;
-    int n = 0;
-    void build(vector<int> & nums, int idx, int start, int end){
-        if(start == end) {
-            seg[idx] = nums[start];
-            return;    
-        }
-        if(start > end) return;
+    vector<int> segTree;
+    vector<int> arr;
+    int n;
 
-        int mid = (start + end) / 2;
-        build(nums, 2 * idx + 1, start, mid);
-        build(nums, 2 * idx + 2, mid + 1, end);
-        seg[idx] = seg[2 * idx + 1] + seg[2 * idx + 2];
-    }
-
-    void update(int idx, int left, int right, int pos, int val){
+    void build(int idx, int left, int right){
         if(left == right) {
-            seg[idx] = val;
+            segTree[idx] = arr[left];
             return;
         }
+        if(left > right) return;
 
         int mid = (left + right) / 2;
-        if(pos <= mid) update(2 * idx + 1, left, mid, pos, val);
-        else update(2 * idx + 2, mid + 1, right, pos, val);
-        seg[idx] = seg[2 * idx + 1] + seg[2 * idx + 2];
+        build(2 * idx + 1, left, mid);
+        build(2 * idx + 2, mid + 1, right);
+        segTree[idx] = segTree[2 * idx + 1] + segTree[2 * idx + 2];
     }
 
-    int query(int idx, int start, int end, int queryLeft, int queryRight){
-        if(queryLeft >end || queryRight < start) return 0;
-        if(queryLeft <= start && queryRight >=end) return seg[idx];
-
+    void update(int idx, int start, int end, int index, int val){
+        if(start == end){
+            segTree[idx] = val;
+            return;
+        }
         int mid = (start + end) / 2;
-        int leftSum = query(2 * idx + 1, start, mid, queryLeft, queryRight);
-        int rightSum = query(2 * idx + 2, mid + 1, end, queryLeft, queryRight);
-        return leftSum + rightSum;
+        if(index <= mid) update(2 * idx + 1, start, mid, index, val);
+        else update(2 * idx + 2, mid + 1, end, index, val);
+        segTree[idx] = segTree[2 * idx + 1] + segTree[2 * idx + 2];
+    }
+
+    int query(int idx, int left, int right, int queryLeft, int queryRight){
+        if(left > queryRight || right < queryLeft) return 0;
+        if(left >= queryLeft && right <= queryRight) return segTree[idx];
+        int mid = (left + right) / 2;
+        int ans = 0;
+        ans += query(2 * idx + 1, left, mid, queryLeft, queryRight);
+        ans += query(2 * idx + 2, mid + 1, right, queryLeft, queryRight);
+        return ans;
     }
 
 public:
     NumArray(vector<int>& nums) {
-        n = nums.size();        
-        seg.resize(4 * n);
-        build(nums, 0, 0, n - 1);
+        n = nums.size();
+        arr = nums;
+        segTree.resize(4 * n);
+        build(0, 0, n - 1);        
     }
     
     void update(int index, int val) {
         update(0, 0, n - 1, index, val);
-        
     }
     
     int sumRange(int left, int right) {
