c6c8e8043d552aa0bafc81ada8d8cddfe5a1f435
Maniteja_Gaddam <happyfamily110705@gmail.com>
2025-09-11 13:55:34 +0530

    Time: 0 ms (100.00%) | Memory: 24.6 MB (93.51%) - LeetSync

--- 2668-form-smallest-number-from-two-digit-arrays/form-smallest-number-from-two-digit-arrays.cpp
+++ 2668-form-smallest-number-from-two-digit-arrays/form-smallest-number-from-two-digit-arrays.cpp
@@ -1,4 +1,14 @@
 class Solution {
+
+private:
+    int smallestEle(vector<int> & nums){
+        int smallest_ele = INT_MAX;
+        for(int ele : nums){
+            smallest_ele = smallest_ele > ele ? ele : smallest_ele;
+        }
+        return smallest_ele;
+    }
+
 public:
     int minNumber(vector<int>& nums1, vector<int>& nums2) {
         sort(nums1.begin(), nums1.end());
@@ -9,7 +19,13 @@ public:
             }
         }
 
-        if(nums1[0] < nums2[0]) return nums1[0] * 10 + nums2[0];
-        return nums2[0] * 10 + nums1[0];
+        int mini1 = smallestEle(nums1);
+        int mini2 = smallestEle(nums2);
+
+        // if(nums1[0] < nums2[0]) return nums1[0] * 10 + nums2[0];
+        // return nums2[0] * 10 + nums1[0];
+
+        if(mini1 < mini2) return mini1 * 10 + mini2;
+        return mini2 * 10 + mini1;
     }
 };
