659fe8bfc9d954b78b9df415171fd7d1da81ba9f
Maniteja_Gaddam <happyfamily110705@gmail.com>
2025-09-17 15:57:30 +0530

    Time: 169 ms (29.92%) | Memory: 181.6 MB (7.20%) - LeetSync

--- 2429-design-a-food-rating-system/design-a-food-rating-system.cpp
+++ 2429-design-a-food-rating-system/design-a-food-rating-system.cpp
@@ -23,26 +23,22 @@ public:
     void changeRating(string food, int newRating) {
         auto [old_rating, cuisine] = food_mpp[food];
 
-        // Remove old rating entry
         mpp[cuisine][old_rating].erase(food);
         if (mpp[cuisine][old_rating].empty()) {
-            mpp[cuisine].erase(old_rating); // Clean up empty rating bucket
+            mpp[cuisine].erase(old_rating); 
         }
 
-        // Add new rating
         mpp[cuisine][newRating].insert(food);
 
-        // Update food mapping
+        
         food_mpp[food] = {newRating, cuisine};
     }
 
     string highestRated(string cuisine) {
         if (mpp.find(cuisine) == mpp.end()) return "";
-
-        // rbegin() -> highest rating
+        
         auto it = mpp[cuisine].rbegin();
-
-        // Lexicographically smallest food for this highest rating
+        
         return *it->second.begin();
     }
 };
