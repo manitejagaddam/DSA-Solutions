35a44e29df007b9fb1a9396db9fc34303bcc2212
Maniteja_Gaddam <happyfamily110705@gmail.com>
2025-09-18 23:36:01 +0530

    Time: 50 ms (27.13%) | Memory: 48.9 MB (43.09%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -13,10 +13,37 @@ private:
         return dp[idx][amount] = min(take, notTake);
     }
 
+    int tabulation(vector<int> coins, int amount){
+        int n = coins.size();
+        const int INF = 1e9; 
+        vector<vector<int>> dp(n, vector<int>(amount + 1, INF));
+        for (int i = 0; i < n; i++) {
+            dp[i][0] = 0;
+        }
+        for (int j = 1; j <= amount; j++) {
+            if (j % coins[0] == 0)
+                dp[0][j] = j / coins[0];
+        }
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j <= amount; j++) {
+                int not_take = dp[i - 1][j]; 
+                int take = INF;
+
+                if (coins[i] <= j)
+                    take = 1 + dp[i][j - coins[i]]; 
+
+                dp[i][j] = min(take, not_take);
+            }
+        }
+
+        return (dp[n - 1][amount] == INF) ? -1 : dp[n - 1][amount];
+    }
+
 public:
     int coinChange(vector<int>& coins, int amount) {
-        vector<vector<int>> dp(coins.size(), vector<int>(amount + 1, -1));
-        int ans = memoization(coins, coins.size() - 1, amount, dp);
-        return ans != INT_MAX ? ans : -1;
+        // vector<vector<int>> dp(coins.size(), vector<int>(amount + 1, -1));
+        // int ans = memoization(coins, coins.size() - 1, amount, dp);
+        // return ans != INT_MAX ? ans : -1;
+        return tabulation(coins, amount);
     }
 };
