f882cf68fda73ce3a18d343326c7633189f18b8d
Maniteja_Gaddam <happyfamily110705@gmail.com>
2025-09-23 13:19:30 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (38.53%) - LeetSync

--- 165-compare-version-numbers/compare-version-numbers.cpp
+++ 165-compare-version-numbers/compare-version-numbers.cpp
@@ -15,12 +15,15 @@ private:
 
     int fetchNum(int & idx, string s){
         int len = s.size();
-        string temp = "";
+        // string temp = "";
+        int num = 0;
         while(idx < len && s[idx] != '.'){
-            temp += s[idx];
+            // temp += s[idx];
+            num = num * 10 + (s[idx] - '0');
             idx++;
         }
-        return convertNum(temp);
+        // return convertNum(temp);
+        return num;
     }
 
 public:
@@ -43,20 +46,6 @@ public:
 
         }
         
-        while(idx1 < len1){
-            num1 = fetchNum(idx1, version1);
-            if(num1 > num2) return 1;
-            else if(num1 < num2) return -1;
-            if (idx1 < len1 && version1[idx1] == '.') idx1++;
-        }
-
-        while(idx2 < len2){
-            num2 = fetchNum(idx2, version2);
-            if(num1 > num2) return 1;
-            else if(num1 < num2) return -1;
-            if (idx2 < len2 && version2[idx2] == '.') idx2++;
-        }
-
         return 0;
 
     }
