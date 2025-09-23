09da6d65ed78918ebe1c138bd5988f6b9b739d8d
Maniteja_Gaddam <happyfamily110705@gmail.com>
2025-09-23 14:44:20 +0530

    Time: 6 ms (42.10%) | Memory: 10.1 MB (19.12%) - LeetSync

---
+++ 2186-count-vowel-substrings-of-a-string/count-vowel-substrings-of-a-string.cpp
@@ -0,0 +1,27 @@
+class Solution {
+
+private:
+    bool isVowel(char ch){
+        return ch == 'a' ||
+               ch == 'e' ||
+               ch == 'i' ||
+               ch == 'o' ||
+               ch == 'u';
+    }
+
+public:
+    int countVowelSubstrings(string word) {
+        int len = word.size();
+        int ans = 0;
+        for(int idx1 = 0 ; idx1 < len ; idx1++){
+            unordered_set<int> st;
+            for(int idx2 = idx1 ; idx2 < len ; idx2++){
+                if(isVowel(word[idx2])) st.insert(word[idx2]);
+                else break;
+                if(st.size() == 5) ans++;
+            }
+        }
+
+        return ans;
+    }
+};
