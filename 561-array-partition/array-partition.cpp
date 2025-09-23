8252b06c9208d5e30700c4b2bd602c0e1a5dfb5b
Maniteja_Gaddam <happyfamily110705@gmail.com>
2025-09-23 18:31:15 +0530

    Time: 5 ms (78.05%) | Memory: 32.2 MB (17.67%) - LeetSync

---
+++ 561-array-partition/array-partition.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int arrayPairSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int ans = 0;
+        // for(int ele : nums){
+        //     ans += min(ele, ele++);
+        // }
+        int len = nums.size();
+        for(int idx = 0 ; idx < len ; idx += 2){
+            ans += min(nums[idx], nums[idx + 1]);
+        }
+        return ans;
+    }
+};
