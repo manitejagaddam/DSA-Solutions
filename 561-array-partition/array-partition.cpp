b332eb48cdf7442a40524526314e24d233e82783
Maniteja_Gaddam <happyfamily110705@gmail.com>
2025-09-23 18:32:44 +0530

    Time: 15 ms (28.53%) | Memory: 32.1 MB (55.16%) - LeetSync

--- 561-array-partition/array-partition.cpp
+++ 561-array-partition/array-partition.cpp
@@ -4,11 +4,12 @@ public:
         sort(nums.begin(), nums.end());
         int ans = 0;
         // for(int ele : nums){
-        //     ans += min(ele, ele++);
+        //     ans += min(ele, ele.next());
         // }
         int len = nums.size();
         for(int idx = 0 ; idx < len ; idx += 2){
-            ans += min(nums[idx], nums[idx + 1]);
+            // ans += min(nums[idx], nums[idx + 1]);
+            ans += nums[idx];
         }
         return ans;
     }
