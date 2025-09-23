7f18a40112fc005a35358bba770599a2b96ebef6
Maniteja_Gaddam <happyfamily110705@gmail.com>
2025-09-23 18:09:06 +0530

    Time: 62 ms (7.12%) | Memory: 32.6 MB (5.52%) - LeetSync

---
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
@@ -0,0 +1,61 @@
+class Solution {
+
+private:
+
+    vector<vector<vector<int>>> dp;
+
+    int backtrack(int idx, vector<int> & nums, int transactionsLeft, bool canBuy, int previousBought){
+        if(idx == nums.size()) return 0;
+        if(!transactionsLeft) return 0;
+
+        int ans = 0;
+
+        if (canBuy) {
+            int buy = backtrack(idx + 1, nums, transactionsLeft, false, nums[idx]) - nums[idx];
+
+            int skip = backtrack(idx + 1, nums, transactionsLeft, true, previousBought);
+
+            ans = max(buy, skip);
+        } 
+        else {
+            int sell = nums[idx] + backtrack(idx + 1, nums, transactionsLeft - 1, true, -1);
+
+            int skip = backtrack(idx + 1, nums, transactionsLeft, false, previousBought);
+
+            ans = max(sell, skip);
+        }
+
+        return ans;
+    }
+
+    int memoization(int idx, vector<int> & nums, int transactionsLeft, bool canBuy, int previousBought){
+        if(idx == nums.size()) return 0;
+        if(!transactionsLeft) return 0;
+
+        if (dp[idx][transactionsLeft][canBuy] != -1) return dp[idx][transactionsLeft][canBuy];
+
+        int ans = 0;
+        if(canBuy){
+            int buy = memoization(idx + 1, nums, transactionsLeft, false, nums[idx]) - nums[idx];
+
+            int skip = memoization(idx + 1, nums, transactionsLeft, true, previousBought);
+
+            ans = max(buy, skip);
+        }else{
+            int sell = memoization(idx + 1, nums, transactionsLeft - 1, true, -1) + nums[idx];
+            int skip = memoization(idx + 1, nums, transactionsLeft, false, previousBought);
+
+            ans = max(sell, skip);
+        }
+
+        return dp[idx][transactionsLeft][canBuy] = ans;
+    }
+
+
+public:
+    int maxProfit(int k, vector<int>& prices) {
+        int len = prices.size();
+        dp.resize(len, vector<vector<int>>(k + 1, vector<int>(2, -1)));
+        return memoization(0, prices, k, true, -1);
+    }
+};
