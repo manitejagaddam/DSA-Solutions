948d3cb7b046a164d740ce30a4a125bbde9b2c0e
Maniteja_Gaddam <happyfamily110705@gmail.com>
2025-09-27 11:47:02 +0530

    Time: 103 ms (5.14%) | Memory: 35.3 MB (5.02%) - LeetSync

--- 830-largest-triangle-area/largest-triangle-area.cpp
+++ 830-largest-triangle-area/largest-triangle-area.cpp
@@ -2,13 +2,7 @@ class Solution {
 
 private:
     double area(vector<int> p1, vector<int> p2, vector<int> p3){
-        double x1 = p1[0];
-        double y1 = p1[1];
-        double x2 = p2[0];
-        double y2 = p2[1];
-        double x3 = p3[0];
-        double y3 = p3[1];
-        return 0.5 * abs(x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2));
+        return 0.5 * abs(p1[0] * (p2[1] - p3[1]) + p2[0] * (p3[1] - p1[1]) + p3[0] * (p1[1] - p2[1]));
     }
 
 public:
@@ -24,9 +18,13 @@ public:
                 }
             }
         }
+
+
         // sort(points.begin(), points.end());
         // if(len % 2 == 0) return max(area(points[0], points[len / 2], points[len - 1]), area(points[0], points[(len + 1) / 2], points[len - 1]));
         // return area(points[0], points[len / 2], points[len - 1]);
+        
+        
         return ans;
         
     }
