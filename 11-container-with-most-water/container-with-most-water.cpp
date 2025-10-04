aad6619e38f763398d561393eca5f0f1947bb677
Maniteja_Gaddam <happyfamily110705@gmail.com>
2025-10-04 10:57:38 +0530

    Time: 1 ms (46.22%) | Memory: 62.8 MB (78.70%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.cpp
+++ 11-container-with-most-water/container-with-most-water.cpp
@@ -1,21 +1,60 @@
 class Solution {
 public:
     int maxArea(vector<int>& height) {
+        // int maxArea = 0;
+        // int len = height.size();
+        // for(int firstLine = 0 ; firstLine < len - 1 ; firstLine++){
+        //     for(int secondLine = firstLine + 1 ; secondLine < len ; secondLine++){
+        //         int width = secondLine - firstLine;
+        //         int height_val = min(height[firstLine], height[secondLine]);
+        //         int tempArea = width * height_val;
+        //         maxArea = maxArea < tempArea ? tempArea : maxArea;
+        //     }
+        // }
 
+        // return maxArea;
+
+
+        // int len = height.size();
+        // vector<int> leftMax(len, 0);
+        // vector<int> rightMax(len, 0);
+        // leftMax[0] = 0;
+        // rightMax[len - 1] = len - 1;
+
+        // for(int idx = 1 ; idx < len ; idx++){
+        //     leftMax[idx] = height[leftMax[idx - 1]] < height[idx] ? idx : leftMax[idx - 1]; 
+        // }
+
+        // for(int idx = len - 2 ; idx >= 0 ; idx--){
+        //     rightMax[idx] = height[rightMax[idx + 1]] < height[idx] ? idx : rightMax[idx + 1];
+        // }
+
+        // int maxArea = 0;
+
+        // for(int idx = 0 ; idx < len ; idx++){
+        //     int width = abs(leftMax[idx] - rightMax[idx]);
+        //     int height_val = min(height[leftMax[idx]], height[rightMax[idx]]);
+
+        //     maxArea = max(maxArea, width * height_val);
+        // }
+
+        // return maxArea;
+
+
+        int len = height.size();
         int left = 0;
-        int right = height.size() - 1;
-        int maxi = 0;
+        int right = len - 1;
+        int maxArea = 0;
         while(left < right){
-            int h = min(height[left], height[right]);
             int width = right - left;
+            int height_val = min(height[left], height[right]);
 
-            maxi = max(maxi, h * width);
+            int tempArea = width * height_val;
 
+            maxArea = max(maxArea, tempArea);
             if(height[left] < height[right]) left++;
             else right--;
         }
-
-        return maxi;
-        
+        return maxArea;
     }
 };
