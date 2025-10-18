bca8ce6ec926cf03ec51ffd5b20bb2c016c41d8b
Maniteja_Gaddam <happyfamily110705@gmail.com>
2025-10-18 13:04:31 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (94.63%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,26 +1,30 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
+        int len = nums.size();
         int start = 0;
-        int end = nums.size() - 1;
+        int end = len - 1;
         while(start <= end){
             int mid = (start + end) / 2;
-            if(nums[mid] == target){
-                return mid;
-            }else if(nums[start] <= nums[mid]){
-                if(nums[start] <= target && nums[mid] >= target){
+    // 		cout << "start : " << start << " end : " << end << endl;
+            if(nums[mid] == target) return mid;
+            
+            
+            // left half is sorted
+            else if(nums[mid] >= nums[start]){
+                // eliminating the right half
+            if(nums[mid] > target && nums[start] <= target){
                     end = mid - 1;
-                }else{
-                    start = mid + 1;
-                }
-            }else{
-                if(nums[mid] <= target && nums[end] >= target){
-                    start = mid + 1;
-                } else{
-                    end = mid - 1;
-                }
+                }else start = mid + 1;
+            }
+            // right half is sorted
+            else{
+                if(nums[mid]  < target && nums[end] >= target){
+                    start = mid + 1;			
+                }else end = mid - 1;
             }
         }
+
         return -1;
     }
 };
