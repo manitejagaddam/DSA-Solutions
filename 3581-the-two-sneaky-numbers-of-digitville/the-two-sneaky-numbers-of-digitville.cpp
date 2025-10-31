b29388475cababc35c3872253c08aad137b0d1fb
Maniteja_Gaddam <happyfamily110705@gmail.com>
2025-10-31 22:31:11 +0530

    Time: 2 ms (45.75%) | Memory: 26.5 MB (15.56%) - LeetSync

--- 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.cpp
+++ 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
     vector<int> getSneakyNumbers(vector<int>& nums) {
-        // vector<int> ans;
-        // unordered_map<int, int> mpp;
-        // for(int ele : nums) mpp[ele]++;
-        // for(auto it : mpp){
-        //     if(it.second == 2) ans.push_back(it.first);
-        // }
-        // return ans;
-
-
         vector<int> ans;
-        unordered_set<int> st;
-        unordered_set<int> inAns;
-        for(int ele : nums){
-            if(st.count(ele) && !inAns.count(ele)){
-                ans.push_back(ele);
-                inAns.insert(ele);
-            }
-            st.insert(ele);
+        unordered_map<int, int> mpp;
+        for(int ele : nums) mpp[ele]++;
+        for(auto it : mpp){
+            if(it.second == 2) ans.push_back(it.first);
         }
         return ans;
+
+
+        // vector<int> ans;
+        // unordered_set<int> st;
+        // unordered_set<int> inAns;
+        // for(int ele : nums){
+        //     if(st.count(ele) && !inAns.count(ele)){
+        //         ans.push_back(ele);
+        //         inAns.insert(ele);
+        //     }
+        //     st.insert(ele);
+        // }
+        // return ans;
     }
 };
