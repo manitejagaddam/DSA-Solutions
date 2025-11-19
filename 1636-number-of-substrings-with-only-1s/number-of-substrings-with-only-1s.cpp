c1f1f02ed47568f11c337855e72cbd3754d801de
Maniteja_Gaddam <happyfamily110705@gmail.com>
2025-11-19 20:40:47 +0530

    Time: 155 ms (5.46%) | Memory: 11.6 MB (65.46%) - LeetSync

---
+++ 1636-number-of-substrings-with-only-1s/number-of-substrings-with-only-1s.cpp
@@ -0,0 +1,55 @@
+const int MOD = 1e9 + 7;
+
+class Solution {
+public:
+    int numSub(string s) {
+        int ans = 0;
+        int end = 0;
+        int len = s.length();
+        while(s[end] == '0') end++;
+        int start = end;
+
+        while(end < len){
+            if(end > 0 && s[end] == '0' && s[end - 1] == '1'){
+                long long count = end - start;
+                cout << ans << " " << count << endl;
+                ans += ((long long)((count * (count + 1)) / 2) % MOD);
+            }else if(end > 0 && s[end] == '1' && s[end - 1] == '0')start = end;
+            end++;
+        }
+
+        if(s[end - 1] == '1'){
+            int count = end - start;
+            ans += ((count * (count + 1)) / 2);
+        }
+
+        return ans;
+
+        // 1111
+        // 1 -> 4
+        // 2 -> 3
+        // 3 -> 2
+        // 4 -> 1
+
+
+        // 11111
+        // 1 -> 5
+        // 2 -> 4
+        // 3 -> 3
+        // 4 -> 2
+        // 5 -> 1
+
+        // 111111
+        // 1 -> 6
+        // 2 -> 5 
+        // 3 -> 4
+        // 4 -> 3
+        // 5 -> 2
+        // 6 -> 1
+
+        //  6 * 7 / 2 -> 3 * 7 = 21
+
+
+        
+    }
+};
