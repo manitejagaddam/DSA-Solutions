8aa5bbf0238f103fe853e0a8d8f3ca5d95fa8b1f
Maniteja_Gaddam <happyfamily110705@gmail.com>
2025-11-22 23:33:04 +0530

    Time: 3 ms (68.03%) | Memory: 9.8 MB (97.46%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cpp
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -1,28 +1,36 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        unordered_set<char> st1;
-        for(auto ch : s1) st1.insert(ch);
+        // unordered_set<char> st1;
+        // for(auto ch : s1) st1.insert(ch);
+        // int len = s2.length();
+        // int k_len = s1.length();
+        // sort(s1.begin(), s1.end());
+        // int idx = 0;
+        // while(idx < len){
+        //     if(st1.count(s2[idx])){
+        //         string temp = s2.substr(idx, k_len);
+        //         sort(temp.begin(), temp.end());
+        //         if(temp == s1) return true;
+        //     }
+        //     idx++;
+        // }
+        // return false;
+
+
+        vector<int> pre1(26, 0), pre2(26, 0);
+        for(char ch : s1) pre1[ch - 'a']++;
+        int k = s1.length();
+        int left = 0;
+        int right = 0;
         int len = s2.length();
-        int k_len = s1.length();
-        sort(s1.begin(), s1.end());
-        int idx = 0;
-        while(idx < len){
-            if(st1.count(s2[idx])){
-                string temp = s2.substr(idx, k_len);
-                sort(temp.begin(), temp.end());
-                if(temp == s1) return true;
-            //     bool notfound = false;
-            //     for(int i = 0 ; i < k_len ; i++){
-            //         if(st1.count(temp[i]) == 0){
-            //             notfound = true;
-            //             idx = i;
-            //             break;
-            //         }
-            //     }
-            //     if(!notfound) return true;
-            }
-            idx++;
+        while(right < len){
+            pre2[s2[right] - 'a']++;
+            if(right >= k) {
+                pre2[s2[right - k] - 'a']--;
+            } 
+            if(pre1 == pre2) return true;
+            right++;
         }
         return false;
     }
