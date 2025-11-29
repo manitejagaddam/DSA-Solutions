f09430b33283b03926024097742b7471afa5bd68
Maniteja_Gaddam <happyfamily110705@gmail.com>
2025-11-29 14:29:12 +0530

    Time: 8 ms (59.81%) | Memory: 239.6 MB (62.15%) - LeetSync

--- 2867-ways-to-split-array-into-good-subarrays/ways-to-split-array-into-good-subarrays.cpp
+++ 2867-ways-to-split-array-into-good-subarrays/ways-to-split-array-into-good-subarrays.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
     int numberOfGoodSubarraySplits(vector<int>& nums) {
-        long long ans = 1, m = 1000000007, count  = 0;
+        long long ans = 1, MOD= 1e9 + 7, count  = 0;
         int i = 0;
         while(i < nums.size() && nums[i] == 0) ++i;
         if(i >= nums.size() ) return 0;
         while(i < nums.size()){
-            if(nums[i] == 1){  ans = (ans * (count +1 ))%m;  count = 0; }
+            if(nums[i] == 1){
+                ans = (ans * (count + 1))  % MOD;
+                count = 0; 
+            }
             else count++;
             i++;
         }
