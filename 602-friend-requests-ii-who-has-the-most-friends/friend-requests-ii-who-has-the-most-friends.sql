01ebcdd84e1970d29ed8ee7f4b3c5280f36eb2f8
Maniteja_Gaddam <happyfamily110705@gmail.com>
2025-11-29 15:11:45 +0530

    Time: 302 ms (44.28%) | Memory: 0B (100.00%) - LeetSync

---
+++ 602-friend-requests-ii-who-has-the-most-friends/friend-requests-ii-who-has-the-most-friends.sql
@@ -0,0 +1,17 @@
+# Write your MySQL query statement below
+WITH all_ids AS (
+   SELECT requester_id AS id 
+   FROM RequestAccepted
+   UNION ALL
+   SELECT accepter_id AS id
+   FROM RequestAccepted)
+SELECT id, num
+FROM 
+   (
+   SELECT id, 
+      COUNT(id) AS num, 
+      RANK () OVER(ORDER BY COUNT(id) DESC) AS rnk
+   FROM all_ids
+   GROUP BY id
+   )t0
+WHERE rnk=1
