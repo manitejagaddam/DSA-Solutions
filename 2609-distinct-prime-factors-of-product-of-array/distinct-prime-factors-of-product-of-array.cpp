d9e618cca6768c610c50d4fd1547a48c57f26b91
Maniteja_Gaddam <happyfamily110705@gmail.com>
2025-12-05 15:25:31 +0530

    Time: 56 ms (13.67%) | Memory: 24.2 MB (10.33%) - LeetSync

---
+++ 2609-distinct-prime-factors-of-product-of-array/distinct-prime-factors-of-product-of-array.cpp
@@ -0,0 +1,44 @@
+class Solution {
+
+private:
+
+    bool isPrime(int n) {
+        if (n <= 3) return true;
+        if (n % 2 == 0 || n % 3 == 0) return false;
+        for (int i = 5; 1LL * i * i <= n; i += 6) {
+            if (n % i == 0 || n % (i + 2) == 0) return false;
+        }
+        return true;
+    }
+
+
+    vector<int> getPrimes(int n){
+        vector<int> primes;
+        primes.push_back(2);
+        primes.push_back(3);
+
+        for(int idx = 5 ; idx <= n ; idx += 2){
+            if(isPrime(idx)) primes.push_back(idx);
+        }
+        return primes;
+    }
+
+public:
+    int distinctPrimeFactors(vector<int>& nums) {
+        vector<int> primes = getPrimes(1000);
+        for(int ele : primes) cout << ele << " ";
+        unordered_set<int> st(primes.begin(), primes.end());
+        unordered_set<int> vis;
+        for(int ele : nums){
+            for(int it : primes){
+                if(it > ele) break;
+                if(ele % it == 0) {
+                    vis.insert(it);
+                    // st.erase(it);
+                }
+            }
+        }
+
+        return vis.size();
+    }
+};
