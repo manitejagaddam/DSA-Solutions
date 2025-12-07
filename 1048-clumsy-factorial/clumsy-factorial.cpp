f1cabde2f110e5fbb62777b1e2c0e26c527ebfa7
Maniteja_Gaddam <happyfamily110705@gmail.com>
2025-12-07 14:49:32 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (43.33%) - LeetSync

--- 1048-clumsy-factorial/clumsy-factorial.cpp
+++ 1048-clumsy-factorial/clumsy-factorial.cpp
@@ -1,20 +1,26 @@
 class Solution {
  public:
-  int clumsy(int N) {
-    if (N == 1)
-      return 1;
-    if (N == 2)
-      return 2;
-    if (N == 3)
-      return 6;
-    if (N == 4)
-      return 7;
-    if (N % 4 == 1)
-      return N + 2;
-    if (N % 4 == 2)
-      return N + 2;
-    if (N % 4 == 3)
-      return N - 1;
-    return N + 1;
+  int clumsy(int n) {
+    // if (N == 1)
+    //   return 1;
+    // if (N == 2)
+    //   return 2;
+    // if (N == 3)
+    //   return 6;
+    // if (N == 4)
+    //   return 7;
+    // if (N % 4 == 1)
+    //   return N + 2;
+    // if (N % 4 == 2)
+    //   return N + 2;
+    // if (N % 4 == 3)
+    //   return N - 1;
+    // return N + 1;
+
+    if(n <= 2) return n;
+    if(n <= 4) return n + 3;
+    if((n-4)%4 == 0) return n+1;
+    else if((n-4)%4 <= 2) return n+2;
+    return n-1;   
   }
 };
