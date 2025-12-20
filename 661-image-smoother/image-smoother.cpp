32b779a531aefd6741dfd44d2edbd4fc01fcf9ba
Maniteja_Gaddam <happyfamily110705@gmail.com>
2025-12-20 23:57:03 +0530

    Time: 12 ms (16.10%) | Memory: 26.1 MB (91.82%) - LeetSync

---
+++ 661-image-smoother/image-smoother.cpp
@@ -0,0 +1,68 @@
+class Solution {
+public:
+    vector<vector<int>> imageSmoother(vector<vector<int>>& img) {
+        int m = img.size();
+        int n = img[0].size();
+
+        vector<int> temp(n);
+        int prevVal = 0;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int sum = 0;
+                int count = 0;
+
+                if (i + 1 < m) {
+                    if (j - 1 >= 0) {
+                        sum += img[i + 1][j - 1];
+                        count += 1;
+                    }
+                    sum += img[i + 1][j];
+                    count += 1;
+                    if (j + 1 < n) {
+                        sum += img[i + 1][j + 1];
+                        count += 1;
+                    }
+                }
+
+                if (j + 1 < n) {
+                    sum += img[i][j + 1];
+                    count += 1;
+                }
+
+                sum += img[i][j];
+                count += 1;
+
+                if (j - 1 >= 0) {
+                    sum += temp[j - 1];
+                    count += 1;
+                }
+
+                if (i - 1 >= 0) {
+                    if (j - 1 >=  0) {
+                        sum += prevVal;
+                        count += 1;
+                    }
+
+                    sum += temp[j];
+                    count += 1;
+
+                    if (j + 1 < n) {
+                        sum += temp[j + 1];
+                        count += 1;
+                    }
+                }
+
+                if (i - 1 >= 0) {
+                    prevVal = temp[j];
+                }
+
+                temp[j] = img[i][j];
+
+                img[i][j] = sum / count;
+            }
+        }
+
+        return img;
+    }
+};
