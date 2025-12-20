2b438168aa9bdd2c117b37d3e6e2e9ac643740c1
Maniteja_Gaddam <happyfamily110705@gmail.com>
2025-12-20 23:57:50 +0530

    Time: 0 ms (100.00%) | Memory: 14.9 MB (29.94%) - LeetSync

---
+++ 674-longest-continuous-increasing-subsequence/longest-continuous-increasing-subsequence.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int findLengthOfLCIS(vector<int>& nums) {
+        int ans = 0, anchor = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (i > 0 && nums[i-1] >= nums[i]) anchor = i;
+            ans = max(ans, i - anchor + 1);
+        }
+        return ans;
+    }
+};
