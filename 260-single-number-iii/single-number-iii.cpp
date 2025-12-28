aae0d576434b3934e1a9978cf9c2c21bc94b4ce3
Maniteja_Gaddam <happyfamily110705@gmail.com>
2025-12-28 11:40:45 +0530

    Time: 3 ms (30.40%) | Memory: 14.7 MB (27.32%) - LeetSync

---
+++ 260-single-number-iii/single-number-iii.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+        unordered_map<int, int> mpp;
+        for(int ele : nums) mpp[ele]++;
+        vector<int> ans;
+        for(auto it : mpp){
+            if(it.second == 1) ans.push_back(it.first);
+        }
+        return ans;
+    }
+};
