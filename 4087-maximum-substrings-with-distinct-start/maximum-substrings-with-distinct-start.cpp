37cd0061cb596c4f903ab629479612ede518b1e9
Maniteja_Gaddam <happyfamily110705@gmail.com>
2026-04-20 13:17:49 +0530

    Time: 4 ms (92.49%) | Memory: 23.7 MB (90.26%) - LeetSync

--- 4087-maximum-substrings-with-distinct-start/maximum-substrings-with-distinct-start.cpp
+++ 4087-maximum-substrings-with-distinct-start/maximum-substrings-with-distinct-start.cpp
@@ -1,7 +1,12 @@
 class Solution {
 public:
     int maxDistinct(string s) {
-        unordered_set<char> st(s.begin(), s.end());
-        return st.size();
+        // unordered_set<char> st(s.begin(), s.end());
+        // return st.size();
+        vector<int> freq(27, 0);
+        for(char ch : s)
+            freq[ch - 'a'] = 1;
+
+        return accumulate(freq.begin(), freq.end(), 0);
     }
 };
