3848da7a983c7ce2194102c46b7431402442fc59
Maniteja_Gaddam <happyfamily110705@gmail.com>
2026-04-20 16:47:38 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (17.58%) - LeetSync

--- 2630-alternating-digit-sum/alternating-digit-sum.cpp
+++ 2630-alternating-digit-sum/alternating-digit-sum.cpp
@@ -5,7 +5,7 @@ public:
         int len = (int)log10(n) + 1;
         int sign = 1;
         if(len % 2 == 0) sign = -1;
-        cout << (int)log10(n) << " " << len;
+        // cout << (int)log10(n) << " " << len;
         int sum = 0;
         while(n){
             sum += (n % 10) * sign;
