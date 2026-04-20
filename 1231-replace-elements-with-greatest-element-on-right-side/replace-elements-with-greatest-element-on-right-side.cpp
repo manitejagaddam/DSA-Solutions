8d231deae0eea5e2101e8fa676fc4bb83d6a55a9
Maniteja_Gaddam <happyfamily110705@gmail.com>
2026-04-20 12:54:20 +0530

    Time: 3 ms (48.17%) | Memory: 72.7 MB (73.12%) - LeetSync

--- 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.cpp
+++ 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.cpp
@@ -1,16 +1,28 @@
 class Solution {
 public:
     vector<int> replaceElements(vector<int>& arr) {
-        int n = arr.size();
-        int lastEle = arr[n - 1];
-        arr[n - 1] = -1;
-        for(int idx = n - 2 ; idx >= 0 ; idx--){
-            int temp = max(arr[idx + 1], lastEle);
+        // int n = arr.size();
+        // int lastEle = arr[n - 1];
+        // arr[n - 1] = -1;
+        // for(int idx = n - 2 ; idx >= 0 ; idx--){
+        //     int temp = max(arr[idx + 1], lastEle);
+        //     lastEle = arr[idx];
+        //     arr[idx] = temp;
+        //     // cout << "temp : " << temp << endl;
+        //     // for(int ele : arr) cout << ele << " ";
+        //     // cout << endl;
+        // }
+        // return arr;
+
+
+        int size = arr.size();
+        int lastEle = arr[size - 1];
+        arr[size - 1] = -1;
+        
+        for(int idx = size - 2 ; idx >= 0 ; idx--){
+            int tempNum = max(arr[idx + 1], lastEle);
             lastEle = arr[idx];
-            arr[idx] = temp;
-            // cout << "temp : " << temp << endl;
-            // for(int ele : arr) cout << ele << " ";
-            // cout << endl;
+            arr[idx] = tempNum;
         }
         return arr;
     }
