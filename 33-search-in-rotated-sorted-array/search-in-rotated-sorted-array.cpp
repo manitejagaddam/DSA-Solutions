dc9790b1421e8a85ec2f5ebafb7c27d8490c201f
Maniteja_Gaddam <happyfamily110705@gmail.com>
2026-05-22 12:31:09 +0530

    Time: 7 ms (0.50%) | Memory: 15.3 MB (33.81%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,26 +1,28 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int len = nums.size();
-        int start = 0;
-        int end = len - 1;
+        // int n {nums.size()};
+        int n = nums.size();
+        int start {0};
+        int end {n - 1};
+
         while(start <= end){
-            int mid = (start + end) / 2;
-    // 		cout << "start : " << start << " end : " << end << endl;
-            if(nums[mid] == target) return mid;
-            
-            
-            // left half is sorted
-            else if(nums[mid] >= nums[start]){
-                // eliminating the right half
-            if(nums[mid] > target && nums[start] <= target){
+
+            int mid {(start + end) / 2};
+            cout << nums[start] << " " << nums[mid] << " " << nums[end] << " " << target << endl;
+            if(nums[mid] == target)return mid;
+
+            // left sorted
+            if(nums[mid] >= nums[start]){
+                if(target >= nums[start] && target < nums[mid]){
                     end = mid - 1;
                 }else start = mid + 1;
             }
-            // right half is sorted
-            else{
-                if(nums[mid]  < target && nums[end] >= target){
-                    start = mid + 1;			
+
+            // right sorted
+            else if(nums[mid] < nums[end]){
+                if(target > nums[mid] && target <= nums[end]){
+                    start = mid + 1;
                 }else end = mid - 1;
             }
         }
