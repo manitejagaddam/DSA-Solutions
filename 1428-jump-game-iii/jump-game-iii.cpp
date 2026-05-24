5177d73dfce864c73c1e83b6c729b351bca38edf
Maniteja_Gaddam <happyfamily110705@gmail.com>
2026-05-24 12:33:15 +0530

    Time: 3 ms (45.37%) | Memory: 40.9 MB (16.92%) - LeetSync

--- 1428-jump-game-iii/jump-game-iii.cpp
+++ 1428-jump-game-iii/jump-game-iii.cpp
@@ -1,6 +1,22 @@
 class Solution {
+
+private:
+    bool memoization(vector<int> & arr, int idx, vector<int> &dp){
+        if(idx < 0 || idx >= arr.size()) return false;
+        if(arr[idx] == 0) return true;
+        if(dp[idx] != -1) return dp[idx];
+        dp[idx] = 0;
+
+        bool ans = memoization(arr, idx + arr[idx], dp) || memoization(arr, idx - arr[idx], dp);
+        return dp[idx] = ans;
+    }
+
 public:
-    bool canReach(vector<int>& A, int i) {
-        return 0 <= i && i < A.size() && A[i] >= 0 && (!(A[i] = -A[i]) || canReach(A, i + A[i]) || canReach(A, i - A[i]));
+    bool canReach(vector<int>& arr, int start) {
+        int size {static_cast<int>(arr.size())};
+        vector<int> dp(size + 1, {-1});
+
+        return memoization(arr, start, dp);
+        
     }
 };
