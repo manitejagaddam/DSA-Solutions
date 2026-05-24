37ea6d2367ae3285cc3bc1854e718c277446ad4e
Maniteja_Gaddam <happyfamily110705@gmail.com>
2026-05-24 11:54:37 +0530

    Time: 640 ms (5.31%) | Memory: 55.8 MB (5.45%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -1,41 +1,39 @@
 class Solution {
 
 private:
-    bool backtrack(vector<int> & nums, int idx){
+    bool recurrsion(vector<int> & nums, int idx){
         if(idx >= nums.size() - 1) return true;
-
-        int temp_idx = nums[idx];
-        while(temp_idx){
-            if(backtrack(nums, idx + temp_idx)) return true;
-            temp_idx--;
+        bool ans {false};
+        for(int temp_idx = idx + 1 ; temp_idx <= idx + nums[idx] ; temp_idx++ ){
+            if(recurrsion(nums, temp_idx)) {
+                ans = true;
+                return true;
+            }
         }
-        return false;
+        return ans;
     }
 
     bool memoization(vector<int> & nums, int idx, vector<int> & dp){
         if(idx >= nums.size() - 1) return true;
-        if(dp[idx] != -1) return dp[idx];
-
-        int temp_idx = nums[idx];
-
-        while(temp_idx){
-            if(memoization(nums, idx + temp_idx, dp)) return dp[idx] = 1;
-            temp_idx--;
+        if(dp[idx] != -1) return dp[idx] == 1;
+        bool ans {false};
+        for(int temp_idx = idx + 1 ; temp_idx <= nums[idx] + idx; temp_idx++){
+            if(memoization(nums, temp_idx, dp)) {
+                ans = true;
+                dp[idx] = 1;
+                return true;
+            }
         }
-
-        return dp[idx] = 0;
+        dp[idx] = ans == true ? 1 : 0;
+        return ans;
     }
-
 
 public:
     bool canJump(vector<int>& nums) {
-
-        // // Backtracking
-        // return backtrack(nums, 0);
-
-
-        // Memoiation
-        vector<int> dp(nums.size() + 1, -1);
+        // return recurrsion(nums, 0);
+        int size {static_cast<int>(nums.size())};
+        vector<int> dp(size + 1, {-1});
         return memoization(nums, 0, dp);
+
     }
 };
