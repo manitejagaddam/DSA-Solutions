af937da9427ddc30fbe86206fd2867dbcb5b1da1
Maniteja_Gaddam <happyfamily110705@gmail.com>
2026-05-24 12:21:49 +0530

    Time: 167 ms (12.31%) | Memory: 22.2 MB (9.91%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,67 +1,25 @@
 class Solution {
 
 private:
-    int backtrack(vector<int> & nums, int idx){
-        if(idx >= nums.size() - 1) return 0;
-
-        int temp_idx = nums[idx];
-        int ans = INT_MAX - 1;
-        while(temp_idx){
-            ans = min(ans, backtrack(nums, idx + temp_idx) + 1); 
-            temp_idx--;
-        }
-        return ans;
-    }
-
     int memoization(vector<int> & nums, int idx, vector<int> & dp){
         if(idx >= nums.size() - 1) return 0;
-
         if(dp[idx] != -1) return dp[idx];
-        int temp_idx = nums[idx];
-        int ans = INT_MAX - 1;
 
-        while(temp_idx) {
-            ans = min(ans, memoization(nums, idx + temp_idx, dp)  + 1);
-            temp_idx--;
+        int size {nums[idx]};
+        int count {INT_MAX - 1};
+        while(size){
+            count = min(count, memoization(nums, idx + size, dp) + 1);
+            size--;
         }
-        return dp[idx] = ans;
+        return dp[idx] = count;
     }
 
 public:
     int jump(vector<int>& nums) {
+        int size {static_cast<int>(nums.size())};
+        vector<int> dp(size + 1, {-1});
 
-        // // BackTracking
-        // int idx = 0;
-        // return backtrack(nums, idx);
+        return memoization(nums, 0, dp);
 
-        // Memoization
-        int idx = 0;
-        vector<int> dp(nums.size(), -1);
-
-        return memoization(nums, idx, dp);
-
-
-
-        
-        // // Greedy Approach
-
-        // int n = nums.size();
-        // if(n == 1) return 0;
-
-        // int jump = 0;
-        // int end = 0;
-        // int far = 0;
-        // for(int i = 0 ; i < n ; i++){
-        //     far = max(far, i + nums[i]);
-
-        //     if(i == end){
-        //         jump++;
-        //         end = far;
-        //         if(end >= n - 1) return jump;
-        //     }
-        // }
-
-        // return jump;
-        
     }
 };
