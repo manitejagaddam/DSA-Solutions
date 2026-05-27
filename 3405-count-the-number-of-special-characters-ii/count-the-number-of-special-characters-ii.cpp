82e6146ca9374b3432ab3d3504be0cf8e6f8cdd4
Maniteja_Gaddam <happyfamily110705@gmail.com>
2026-05-27 12:17:08 +0530

    Time: 92 ms (27.25%) | Memory: 25.7 MB (23.31%) - LeetSync

--- 3405-count-the-number-of-special-characters-ii/count-the-number-of-special-characters-ii.cpp
+++ 3405-count-the-number-of-special-characters-ii/count-the-number-of-special-characters-ii.cpp
@@ -15,12 +15,9 @@ public:
         }
         
         int count = 0;
-        // Check letters 'a' through 'z'
         for (char c = 'a'; c <= 'z'; ++c) {
             char upper_c = toupper(c);
-            // Check if both exist
             if (last_lower.count(c) && first_upper.count(upper_c)) {
-                // Condition: last lowercase index < first uppercase index
                 if (last_lower[c] < first_upper[upper_c]) {
                     count++;
                 }
