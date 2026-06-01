7a2de077c5e97378308338232d7c24242e157426
Maniteja_Gaddam <happyfamily110705@gmail.com>
2026-06-01 09:40:36 +0530

    Time: 23 ms (1.69%) | Memory: 16.8 MB (12.55%) - LeetSync

---
+++ 2248-minimum-cost-of-buying-candies-with-discount/minimum-cost-of-buying-candies-with-discount.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minimumCost(vector<int>& cost) {
+        sort(cost.rbegin(), cost.rend());
+        for(int ele : cost) cout << ele << " ";
+        cout << endl;
+        int count{0};
+        int sum {0};
+        int len {static_cast<int>(cost.size())};
+
+        for(int idx = 0 ; idx < len ; idx++){
+            if(count == 2) {
+                count = 0;
+                continue;
+            }
+            
+            count++;
+            sum += cost[idx];
+        }
+        
+        return sum;
+    }
+};
