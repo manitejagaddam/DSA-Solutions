8d8712d53d11c21070e5e96431681665a3971f47
Maniteja_Gaddam <happyfamily110705@gmail.com>
2026-06-01 09:41:55 +0530

    Time: 25 ms (1.69%) | Memory: 17 MB (7.87%) - LeetSync

--- 2248-minimum-cost-of-buying-candies-with-discount/minimum-cost-of-buying-candies-with-discount.cpp
+++ 2248-minimum-cost-of-buying-candies-with-discount/minimum-cost-of-buying-candies-with-discount.cpp
@@ -4,17 +4,18 @@ public:
         sort(cost.rbegin(), cost.rend());
         for(int ele : cost) cout << ele << " ";
         cout << endl;
-        int count{0};
+        // int count{0};
         int sum {0};
         int len {static_cast<int>(cost.size())};
 
         for(int idx = 0 ; idx < len ; idx++){
-            if(count == 2) {
-                count = 0;
-                continue;
-            }
+            // if(count == 2) {
+            //     count = 0;
+            //     continue;
+            // }
+            if((idx + 1) % 3 == 0) continue;
             
-            count++;
+            // count++;
             sum += cost[idx];
         }
         
